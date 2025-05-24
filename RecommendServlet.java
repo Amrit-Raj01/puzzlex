4b950ee6be2518fc093abce82749fe41754fa7df
Amrit-Raj01 <amritrajsingh951@gmail.com>
2025-05-24 20:30:52 +0530

    Create RecommendServlet.java

---
+++ RecommendServlet.java
@@ -0,0 +1,30 @@
+import model.User;
+import model.Song;
+import service.SongDatabase;
+import service.RecommendationService;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.util.List;
+
+@WebServlet("/recommend")
+public class RecommendServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+        throws ServletException, IOException {
+
+        String name = request.getParameter("name");
+        String mood = request.getParameter("mood");
+
+        User user = new User(name, mood);
+        SongDatabase db = new SongDatabase();
+        RecommendationService recommender = new RecommendationService(db);
+
+        List<Song> recommendations = recommender.recommend(user);
+        request.setAttribute("recommendations", recommendations);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("result.jsp");
+        dispatcher.forward(request, response);
+    }
+}
