3e8ad9d84f6fe88855f0154013ad3d30f3594802
Amrit-Raj01 <amritrajsingh951@gmail.com>
2025-05-24 11:49:30 +0530

    Update and rename index.html to Song.java

---
+++ Song.java
@@ -0,0 +1,11 @@
+package model;
+
+public class Song {
+    private final String title;
+    private final String singer;
+    private final String movie;
+    private final String mood;
+    
+    // Constructor + Getters
+    // toString() implementation
+}
