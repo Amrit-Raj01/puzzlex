0a003e7ec86dc8bb468443b11ab65f4de75c0325
Amrit-Raj01 <amritrajsingh951@gmail.com>
2025-05-24 20:25:16 +0530

    Update and rename BollywoodSongRecommender.java to index.html

--- BollywoodSongRecommender.java
+++
@@ -1,9 +0,0 @@
-import service.SongRecommender;
-import database.BollywoodSongDatabase;
-import ui.ConsoleUI;
-
-public class BollywoodSongRecommender {
-    public static void main(String[] args) {
-        // Initialization code
-    }
-}
