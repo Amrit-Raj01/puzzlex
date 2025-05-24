e191d93615646c1d5e500c8edfc30507c38f714d
Amrit-Raj01 <amritrajsingh951@gmail.com>
2025-05-24 11:55:31 +0530

    Update and rename style.css to service.java

---
+++ service.java
@@ -0,0 +1,11 @@
+package service;
+
+import database.BollywoodSongDatabase;
+import model.Song;
+import java.util.List;
+
+public class SongRecommender {
+    private final BollywoodSongDatabase database;
+    
+    // Constructor + recommendSongs() method
+}
